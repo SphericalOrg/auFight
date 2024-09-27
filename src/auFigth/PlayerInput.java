fac6901a18683f2efbdba0bc95daf548a03b828a
Strawberry Frappé <j.saez18@ufromail.cl>
2024-09-27 12:24:42 -0300

    Create PlayerInput.java

---
+++ src/auFigth/PlayerInput.java
@@ -0,0 +1,13 @@
+package auFigth;
+
+public abstract class PlayerInput {
+    public boolean hasPlayer = false;
+    public float LEFT;
+    public float RIGHT;
+    public float UP;
+    public float DOWN;
+    public boolean A;
+    public boolean B;
+    public void update(){}
+    public boolean interacted(){return false;}
+}
