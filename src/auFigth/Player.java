c4ff6e8757cdd3c57c955218c4c56a7ec877ca38
Strawberry Frappé <j.saez18@ufromail.cl>
2024-09-27 12:41:49 -0300

    Create Player.java

---
+++ src/auFigth/Player.java
@@ -0,0 +1,14 @@
+package auFigth;
+
+public class Player extends Entity {
+    private PlayerInput controller;
+    private boolean performingAction;
+    public int score;
+    
+    public void summonShield(){
+    }
+
+    public void summonWeapon(){
+
+    }
+}
