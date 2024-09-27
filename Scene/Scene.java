ffc5bb215ca6dbffade965469f145d73a10defbc
Ezio <133083000+DaniAuditore@users.noreply.github.com>
2024-09-27 12:35:26 -0300

    feat: crear ventana básica en la interfaz

--- Scene/Scene.java
+++ Scene/Scene.java
@@ -1,6 +1,11 @@
+import javax.swing.*;
 
 public class Scene {
     public static void main(String[] args) {
-        System.out.println("Hola, estupidos");
+        JFrame f=new JFrame();
+        f.setBounds(f.getBounds());
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 }
