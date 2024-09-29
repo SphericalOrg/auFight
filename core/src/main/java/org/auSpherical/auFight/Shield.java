19f13c9781efc3bc519ab3af5a789712fd1bcba1
Strawberry Frappé <65193599+StrawberryFrappe@users.noreply.github.com>
2024-09-29 01:44:45 -0300

    Update Shield.java

--- core/src/main/java/org/auSpherical/auFight/Shield.java
+++ core/src/main/java/org/auSpherical/auFight/Shield.java
@@ -5,11 +5,11 @@ public class Shield extends Entity implements Damageable{
     private boolean isActive;
 
 
-    public regenerar(){
+    public void regenerar(){
        //TODO: Waiting
     }
 
-    public recieveDamage(){
+    public void recieveDamage(){
         //TODO: Waiting
     }
 }
