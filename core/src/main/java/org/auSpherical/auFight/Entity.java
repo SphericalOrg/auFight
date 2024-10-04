9a01d343e00ab101761f8d3a312e39f50b94165b
Strawberry Frappé <j.saez18@ufromail.cl>
2024-10-04 11:28:26 -0300

    Update Entity.java

--- core/src/main/java/org/auSpherical/auFight/Entity.java
+++ core/src/main/java/org/auSpherical/auFight/Entity.java
@@ -1,9 +1,11 @@
 package org.auSpherical.auFight;
 
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
 public abstract class Entity {
     private int health;
-    private int sprite;
-    private int collitionBox;
+    private Sprite sprite;
+    private CollitionBox collitionBox;
 
     public void move(){}
 }
