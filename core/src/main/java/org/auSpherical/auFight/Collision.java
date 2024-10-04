6bc7ba50fb8b5e687346d5e1efc94bfb22e88ec5
cris <c.cariqueo03@ufromail.cl>
2024-10-04 15:22:24 -0300

    fix: collision-related classes name corrected

---
+++ core/src/main/java/org/auSpherical/auFight/Collision.java
@@ -0,0 +1,14 @@
+package org.auSpherical.auFight;
+
+public class Collision {
+    private Collidable A;
+    private Collidable B;
+
+    public void collision(Collidable A, Collidable B) {
+
+    }
+
+    public void resolveCollision() {
+
+    }
+}
