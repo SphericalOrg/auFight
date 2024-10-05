1611bb91589f09b09f39a981e46beada45c0b634
Strawberry Frappé <65193599+StrawberryFrappe@users.noreply.github.com>
2024-10-05 06:49:11 -0300

    Update Entity.java

--- core/src/main/java/org/auSpherical/auFight/Entity.java
+++ core/src/main/java/org/auSpherical/auFight/Entity.java
@@ -1,14 +1,14 @@
 package org.auSpherical.auFight;
 
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public abstract class Entity {
     public int health;
     //Use textureRegion por que para despues nos puede servir para animaciones
-    public TextureRegion texture;
+    public TextureRegion region;
     public CollitionBox collitionBox;
+    public Sprite sprite;
 
     public void move(){}
 }
