4fd4682f87b5e265d05103484ed79793119feda4
Strawberry Frappé <65193599+StrawberryFrappe@users.noreply.github.com>
2024-10-05 06:49:16 -0300

    Update GameLogicManager.java

--- core/src/main/java/org/auSpherical/auFight/GameLogicManager.java
+++ core/src/main/java/org/auSpherical/auFight/GameLogicManager.java
@@ -9,15 +9,24 @@ public class GameLogicManager {
 
     public GameLogicManager(AuFight main) {
         this.main = main;
-        player1 = new Player();
-        player2 = new Player();
+        player1 = new Player(new KeyboardInput(true),1);
+        player2 = new Player(new KeyboardInput(false),2);
     }
     //aqui va donde deberias actualizar la logica del juego
     public void update(float delta) {
+
         handleInput();
     }
     //no me acuerdo como xuxa cmanejabas los inputs jun asi que dejo esto por aca si lo usas bien si no borralo
     private void handleInput() {
-
+        player1.move();
+        player2.move();
+        if (player1.sprite.getX()>player2.sprite.getX()){
+            player1.lookLeft();
+            player2.lookRight();
+        } else {
+            player1.lookRight();
+            player2.lookLeft();
+        }
     }
 }
