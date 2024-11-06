f962febfb1b8b966238223b4399aae31faf8c370
bfl-dev <paparrasilva2@gmail.com>
2024-11-06 12:05:35 -0300

    feat: add placeholder test

---
+++ core/src/test/java/org/auSpherical/auFight/data/LeaderManagerTest.java
@@ -0,0 +1,14 @@
+package org.auSpherical.auFight.data;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeaderManagerTest {
+
+    @Test
+    void hola() {
+        Assertions.assertTrue(true);
+    }
+}
