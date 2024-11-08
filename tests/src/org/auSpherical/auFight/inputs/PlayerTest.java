cef0f249caf28101da153cb4cd098ef57684bc05
F33gz <127645044+F33gz@users.noreply.github.com>
2024-11-08 12:26:50 -0300

    fix: change unit test

--- tests/src/org/auSpherical/auFight/inputs/PlayerTest.java
+++ tests/src/org/auSpherical/auFight/inputs/PlayerTest.java
@@ -8,24 +8,15 @@ import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
 
-    private static Player player;
-    private static PlayerInput mockInput;
+    private  Player player;
+    private  PlayerInput mockInput;
 
-    @BeforeAll
-    public static void setUp() {
+    @BeforeEach
+    public void setUp() {
         mockInput = new PlayerInput();
         player = new Player(mockInput, 1);
     }
 
-    @BeforeEach
-    public void reset() {
-        mockInput.LEFT = 0;
-        mockInput.RIGHT = 0;
-        mockInput.UP = 0;
-        mockInput.DOWN = 0;
-        mockInput.A = false;
-        mockInput.B = false;
-    }
 
     @Test
     public void testInitialPosition() {
@@ -50,6 +41,7 @@ public class PlayerTest {
     @Test
     public void testJump() {
         mockInput.UP = 1;
+        player.actionable = true;
         player.move();
         assertTrue(player.getSpeed().y > 0);
     }
@@ -57,7 +49,11 @@ public class PlayerTest {
     @Test
     public void testDoubleJump() {
         mockInput.UP = 1;
-        player.move();
+        player.actionable = true;
+        for (int i = 0; i < 60; i++) {
+            player.move();
+        }
+        mockInput.UP = 1;
         player.move(); // Simulate second jump
         assertFalse(player.doubleJump);
     }
