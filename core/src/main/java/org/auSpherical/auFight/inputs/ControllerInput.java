880727cd0cb43066ddb4461046b1b6626b26dfd9
Strawberry Frappé <65193599+StrawberryFrappe@users.noreply.github.com>
2024-11-09 21:55:26 -0300

    Update ControllerInput.java

--- core/src/main/java/org/auSpherical/auFight/inputs/ControllerInput.java
+++ core/src/main/java/org/auSpherical/auFight/inputs/ControllerInput.java
@@ -6,8 +6,8 @@ import com.github.strikerx3.jxinput.enums.XInputButton;
 import com.github.strikerx3.jxinput.exceptions.XInputNotLoadedException;
 
 public class ControllerInput extends PlayerInput {
-    private final double DEAD_ZONE = 0.133;
-    private final double TRIGGER_DEAD_ZONE = 0.3;
+    private static final double DEAD_ZONE = 0.133;
+    private static final double TRIGGER_DEAD_ZONE = 0.3;
     public final XInputDevice DEVICE;
     private final XInputAxes AXES;
     private XInputButtons BUTTONS;
@@ -26,7 +26,7 @@ public class ControllerInput extends PlayerInput {
     }
     @Override
     public void update(){
-        poll();
+        DEVICE.poll();
         setInputValues();
     }
     @Override
@@ -34,54 +34,63 @@ public class ControllerInput extends PlayerInput {
         update();
         return !hasPlayer && anyPressed();
     }
-    private boolean anyPressed() {
-        return BUTTONS.a || BUTTONS.b || BUTTONS.x || BUTTONS.y || BUTTONS.start || BUTTONS.back || BUTTONS.up || BUTTONS.down || BUTTONS.left || BUTTONS.right || BUTTONS_DELTA.isPressed(XInputButton.LEFT_SHOULDER) || BUTTONS_DELTA.isPressed(XInputButton.RIGHT_SHOULDER) || BUTTONS_DELTA.isPressed(XInputButton.A) || BUTTONS_DELTA.isPressed(XInputButton.B) || BUTTONS_DELTA.isPressed(XInputButton.X) || BUTTONS_DELTA.isPressed(XInputButton.Y) || BUTTONS_DELTA.isPressed(XInputButton.START) || BUTTONS_DELTA.isPressed(XInputButton.BACK);
+    private boolean anyPressed() {return anyButtonsPressed() || anyDirectionsPressed() || anyTriggersPressed();}
+
+    private boolean anyTriggersPressed(){
+        return BUTTONS_DELTA.isPressed(XInputButton.LEFT_SHOULDER)||BUTTONS_DELTA.isPressed(XInputButton.RIGHT_SHOULDER)
+                || getTrigger(XInputAxis.LEFT_TRIGGER) || getTrigger(XInputAxis.RIGHT_TRIGGER);
     }
+
+    private boolean anyButtonsPressed(){
+        return BUTTONS.a || BUTTONS.b || BUTTONS.x || BUTTONS.y || BUTTONS.start || BUTTONS.back ||
+                BUTTONS_DELTA.isPressed(XInputButton.A) || BUTTONS_DELTA.isPressed(XInputButton.B) ||
+                BUTTONS_DELTA.isPressed(XInputButton.X) || BUTTONS_DELTA.isPressed(XInputButton.Y) ||
+                BUTTONS_DELTA.isPressed(XInputButton.START) || BUTTONS_DELTA.isPressed(XInputButton.BACK);
+    }
+
+    private boolean anyDirectionsPressed(){
+        return BUTTONS.up || BUTTONS.down || BUTTONS.left || BUTTONS.right ||
+                BUTTONS_DELTA.isPressed(XInputButton.DPAD_UP) || BUTTONS_DELTA.isPressed(XInputButton.DPAD_DOWN) ||
+                BUTTONS_DELTA.isPressed(XInputButton.DPAD_LEFT) || BUTTONS_DELTA.isPressed(XInputButton.DPAD_RIGHT);
+    }
+
     private void setInputValues() {
+        updateButtonsValues();
+        updateDirectionalValues();
+        updateTriggersValues();
+    }
+
+    private void updateButtonsValues(){
         A = BUTTONS_DELTA.isPressed(XInputButton.A);
         B = BUTTONS_DELTA.isPressed(XInputButton.B);
-        leftRight();
-        upDown();
-        triggers();
-    }
-    private void poll(){
-        DEVICE.poll();}
-    private void leftRight(){
-        if (valueDeltaX()>0){
-            RIGHT = valueDeltaX();
-            LEFT = 0;
-        } else if (valueDeltaX()<0) {
-            LEFT = -valueDeltaX();
-            RIGHT = 0;
-        } else {
-            LEFT = 0;
-            RIGHT = 0;
-        }
-    }
-    private void upDown(){
-        if (valueDeltaY()>0){
-            UP = valueDeltaY();
-            DOWN = 0;
-        } else if (valueDeltaY()<0) {
-            DOWN = -valueDeltaY();
-            UP = 0;
-        } else {
-            UP = 0;
-            DOWN = 0;
-        }
     }
 
-    private void triggers(){
+    private void updateDirectionalValues(){
+        updateHorizontalValues();
+        updateVerticalValues();
+    }
+
+    private void updateVerticalValues(){
+        UP = Math.max(valueDeltaY(), 0);
+        DOWN = Math.max(-valueDeltaY(), 0);
+    }
+
+    private void updateHorizontalValues(){
+        RIGHT = Math.max(valueDeltaX(), 0);
+        LEFT = Math.max(-valueDeltaX(), 0);
+    }
+
+    private void updateTriggersValues(){
         LB = BUTTONS_DELTA.isPressed(XInputButton.LEFT_SHOULDER);
-
-        LT = leftTriggerFuse && AXES.get(XInputAxis.LEFT_TRIGGER)>= TRIGGER_DEAD_ZONE;
-        leftTriggerFuse = !LT && AXES.get(XInputAxis.LEFT_TRIGGER)< TRIGGER_DEAD_ZONE;
-
         RB = BUTTONS_DELTA.isPressed(XInputButton.RIGHT_SHOULDER);
 
-        RT = rightTriggerFuse && AXES.get(XInputAxis.RIGHT_TRIGGER) >= TRIGGER_DEAD_ZONE;
-        rightTriggerFuse = !RT && AXES.get(XInputAxis.RIGHT_TRIGGER) < TRIGGER_DEAD_ZONE;
+        LT = leftTriggerFuse && getTrigger(XInputAxis.LEFT_TRIGGER);
+        leftTriggerFuse = !LT && !getTrigger(XInputAxis.LEFT_TRIGGER);
+
+        RT = rightTriggerFuse && getTrigger(XInputAxis.RIGHT_TRIGGER);
+        rightTriggerFuse = !RT && !getTrigger(XInputAxis.RIGHT_TRIGGER);;
     }
+    private boolean getTrigger(XInputAxis axis){return AXES.get(axis) >= TRIGGER_DEAD_ZONE;}
     private float valueDeltaX(){return dPadX()!=0? dPadX():leftStickX();}
     private float valueDeltaY(){return dPadY()!=0? dPadY():leftStickY();}
     private float dPadY(){return Boolean.compare(BUTTONS.up, BUTTONS.down);}
