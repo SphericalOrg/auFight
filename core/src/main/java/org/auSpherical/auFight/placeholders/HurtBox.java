4de768550b06b2be63ae656ab12a4420b52e64f5
Strawberry Frappé <65193599+StrawberryFrappe@users.noreply.github.com>
2024-12-16 05:29:57 -0300

    Update HurtBox.java

--- core/src/main/java/org/auSpherical/auFight/placeholders/HurtBox.java
+++ core/src/main/java/org/auSpherical/auFight/placeholders/HurtBox.java
@@ -6,26 +6,24 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import org.auSpherical.auFight.inputs.Player;
 
-public class HurtBox extends Rectangle implements Damageable {
-    private final Vector2 vector2;
+public class HurtBox extends Rectangle implements Damageable, CollitionBox {
     private final Player player;
 
     public HurtBox(Vector2 vector2, Player player) {
         super(vector2.x, vector2.y, 68, 113);
-        this.vector2 = vector2;
         this.player = player;
     }
 
     public void update() {
-        setPosition(vector2.x-22, vector2.y-43);
+        setPosition(player.getPosition());
     }
 
     public Player getPlayer() {
         return player;
     }
 
-    public void receiveDamage(float damage) {
-        player.receiveDamage(damage);
+    public int receiveDamage(float damage) {
+        return player.receiveDamage(damage);
     }
 
     //si quieres ver la hitbox en pantalla
